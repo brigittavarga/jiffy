4595624dde64f84c2c50c398083bf8692cf1fec2
Brigitta Varga <v.brigitta.zs@gmail.com>
2021-05-24 12:11:58 +0200

    added <video>

--- jiffy.js
+++ jiffy.js
@@ -1,5 +1,18 @@
+function createVideo (src) {
+    const video = document.createElement("video")
+    // here we can set attributes on our created element using the . notation 
+    video.src = src
+    video.autoplay = true
+    video.loop = true
+
+    console.log(video)
+    // when we use return we tell the function to give us something back
+    return video
+
+}
+
 // here we put our URL into fetch
-fetch("https://api.giphy.com/v1/gifs/search?api_key=Jar9pAn2Ia0OVPjpsRuwSB3pQUXiq4bU=doggo&limit=50&offset=0&rating=PG-13&lang=en")
+fetch("https://api.giphy.com/v1/gifs/search?api_key=Jar9pAn2Ia0OVPjpsRuwSB3pQUXiq4bU&q=doggo&limit=50&offset=0&rating=PG-13&lang=en")
     // here we use .then() to handle the response
     .then(response => { 
     // here we need to convert our response to json 
@@ -10,16 +23,13 @@ fetch("https://api.giphy.com/v1/gifs/search?api_key=Jar9pAn2Ia0OVPjpsRuwSB3pQUXi
     const gif = json.data[0]
     // here we look beside of the first result and grab the original mp4 source
     const src = gif.images.original.mp4
+    
     console.log(src)
 
-    const video = document.createElement("video")
-    // here we can set attributes on our created element using the . notation 
-    video.src = src
-    video.autoplay = true
-    video.loop = true
+    const video = createVideo(src)
 
     // we grab our videos element and then append our newly created video to it
-    const videos = document.querySelector(".videos")
+    const videosEl = document.querySelector(".videos")
     videosEl.appendChild("video")
 
     }) 
